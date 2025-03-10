1be2c63bd9f7466afdd2f55aa1a65b9426484554
Jonaathan Harrington <jonathan@harringtons.me>
2025-03-10 16:09:16 +0000

    Updates

--- html/app.js
+++ html/app.js
@@ -1,19 +1,21 @@
 
 var app = new Framework7({
-    // App root element
-    el: '#app',
-    // App Name
-    name: 'My App',
-    // Enable swipe panel
-    panel: {
-      swipe: true,
+  // App root element
+  el: '#app',
+  // App Name
+  name: 'Fixtures',
+  // Enable swipe panel
+  panel: {
+    swipe: true,
+  },
+  // Add default routes
+  routes: [
+    {
+      path: '/about/',
+      url: 'about.html',
     },
-    // Add default routes
-    routes: [
-      {
-        path: '/about/',
-        url: 'about.html',
-      },
-    ],
-    // ... other parameters
-  });
+  ],
+  // ... other parameters
+});
+
+var mainView = app.views.create('.view-main');
--- src/kiltha_to_html.sh
+++ src/kiltha_to_html.sh
@@ -1,7 +1,9 @@
+cp html/app.js docs/
+
 cat html/kilta_header.html > docs/kiltha.html
 
 cat docs/kog.json | jq -r '.fixtures[] | 
-    "<li>
+    "<li class=\"custom-media-item\">
         <div class=\"item-content\">
             <div class=\"item-media\">
                 <img width=\"70\" style=\"border-radius: 8px\" src=\"" + .crest + "\" />
@@ -9,6 +11,7 @@ cat docs/kog.json | jq -r '.fixtures[] |
             <div class=\"item-inner\">
                 <div class=\"item-title-row\">
                     <div class=\"item-title\">" + .date + ", " + .time + "</div>
+                    <div class=\"item-after\">" + .age + "</div>
                 </div>
                 <div class=\"item-subtitle\">" + .opposition + " in " + .venue + "</div>
                 <div class=\"item-text\">" + .compname + "</div>
