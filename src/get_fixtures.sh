e2efd40fe5f037d9e8b84c86757a27bba9b62a52
jonathan <jonathan>
2025-03-21 15:24:26 +0000

    Add nicer date format

--- src/get_fixtures.sh
+++ src/get_fixtures.sh
@@ -26,33 +26,42 @@ curl -s "$URL" | \
             venue: @(data-venue),
             compname: @(data-compname)            
     }]
-}" | jq --arg team "$TEAM_NAME" '.fixtures |= map(
-    .dt = try (.date + " " + .time | strptime("%d %b %Y %H:%M") | strftime("%Y-%m-%d %H:%M")) catch ("") |
-    .unix = try (.date + " " + .time | strptime("%d %b %Y %H:%M") | mktime) catch ("") |
-    if .hometeam | test($team; "i") then
-            .opposition = .awayteam |
-            .crest = .awaycrest
-    elif .awayteam | test($team; "i") then
-            .opposition = .hometeam |
-            .crest = .homecrest
-    else
-            .                
-    end | 
-    .age = if .compname | test("Fe12"; "i") then 
-            "U12" 
-    elif .compname | test("Fe13"; "i") then 
-            "U13" 
-    elif .compname | test("Fe14"; "i") then 
-            "U14" 
-    elif .compname | test("Fe14"; "i") then 
-            "U15" 
-    elif .compname | test("Fe16"; "i") then 
-            "U16" 
-    elif .compname | test("Fe17"; "i") then 
-            "U17" 
-    elif .compname | test("Fe18"; "i") then 
-            "Minor" 
-    else
-            "Adult" 
-    end          
+}" | jq --arg team "$TEAM_NAME" '
+  def ordinal:
+    if . == 1 or . == 21 or . == 31 then . | tostring + "st"
+    elif . == 2 or . == 22 then . | tostring + "nd"
+    elif . == 3 or . == 23 then . | tostring + "rd"
+    else . | tostring + "th"
+    end;
+
+    .fixtures |= map(
+        .dt = try (.date + " " + .time | strptime("%d %b %Y %H:%M") | strftime("%Y-%m-%d %H:%M")) catch ("") |
+        .unix = try (.date + " " + .time | strptime("%d %b %Y %H:%M") | mktime) catch ("") |
+        .date |= (strptime("%d %b %Y") | strftime("%a") as $dayOfWeek |  strftime("%-d") | tonumber as $dayNum | $dayOfWeek + " " + ($dayNum | ordinal)) |
+        if .hometeam | test($team; "i") then
+                .opposition = .awayteam |
+                .crest = .awaycrest
+        elif .awayteam | test($team; "i") then
+                .opposition = .hometeam |
+                .crest = .homecrest
+        else
+                .                
+        end | 
+        .age = if .compname | test("Fe12"; "i") then 
+                "U12" 
+        elif .compname | test("Fe13"; "i") then 
+                "U13" 
+        elif .compname | test("Fe14"; "i") then 
+                "U14" 
+        elif .compname | test("Fe14"; "i") then 
+                "U15" 
+        elif .compname | test("Fe16"; "i") then 
+                "U16" 
+        elif .compname | test("Fe17"; "i") then 
+                "U17" 
+        elif .compname | test("Fe18"; "i") then 
+                "Minor" 
+        else
+                "Adult" 
+        end          
 )'
